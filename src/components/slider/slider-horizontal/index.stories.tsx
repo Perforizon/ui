854d423e2f77d20c6f77dc312b370f90a7f61eda
Liam <lmmurray@student.fullsail.edu>
2020-05-09 19:45:30 -0400

    sets wrapper default position mode to relative and adds props.children to horizontal slider

--- src/components/slider/slider-horizontal/index.stories.tsx
+++ src/components/slider/slider-horizontal/index.stories.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import {HorizontalSlider} from '.';
 import outerCursorSVGUrl from "./outerCursor.svg";
+import {OpacityCanvas} from "@perforizon/colorpicker";
+
+import "../../../styles/no-select.scss";
 
 export default {
   title: "Slider"
@@ -15,56 +18,80 @@ export const Horizontal = () =>{
         scale: 2/10
     }
 }
-  const [input, setInput] = useState(0);
+  const [input, setInput] = useState(1);
   return (
     <div>
       <HorizontalSlider 
         setInput={setInput} 
-        barHeight={10}
-        barWidth={100}
+        barHeight={20}
+        barWidth={200}
         innerCursorWidth={8} 
-        outerCursorWidth={10} 
+        outerCursorWidth={15} 
         barStyle={{backgroundColor: "red"}}
         innerCursorStyle={{backgroundColor: "rgba(240,240,240,1"}}
         outerCursorStyle={{backgroundColor: "rgba(0,0,0,0)"}}
         outerCursorSVGUrl={outerCursorSVGUrl}
       />
-      {"input: " + input.toFixed(4)}
+      <br/>
+      <br/>
+      <text className={"no-select"}>
+        {"input: " + input.toFixed(4)}
+      </text>
     </div>
   )
 };
 
-export const HorizontalStyled = () =>{ 
+export const HorizontalAhabStyle = () =>{ 
   const innerCursorVariants = {
     default : {
+        boxShadow:"0px 0px 25px 5px rgba(255,255,255,0)",
         scaleX: 1
     },
     highlight : {
-        scaleX: 2
+        boxShadow:"0px 0px 25px 2px rgba(255,255,255,1)",
+        scaleX: 2,
+        transition: {
+          type: "tween",
+          duration: .1
+        }
     }
   }
   const outerCursorVariants = {
       default : {
+          filter: "invert(0.0)",
+          opacity: .7,
           scale : 1
       },
       highlight : {
-          scale: 2
+          filter: "invert(1)",
+          opacity: 1,   
+          scale: 2,
+          transition: {
+            type: "tween",
+            duration: .1
+          }
       }
   }
-  const [input, setInput] = useState(0);
+  const fillVariants = {
+    default : {
+      backgroundColor: "rgba(104,106,106,1)"
+    },
+    highlight : {
+      backgroundColor: "rgba(200,200,200,1)"
+  }
+  }
+  const [input, setInput] = useState(1);
   return (
     <div>
       <HorizontalSlider 
         setInput={setInput} 
-        barHeight={15}
-        barWidth={140}
+        barHeight={30}
+        barWidth={280}
         innerCursorWidth={5} 
         outerCursorWidth={15} 
+        fillVariants={fillVariants}
         outerCursorVariants={outerCursorVariants}
         innerCursorVariants={innerCursorVariants}
-        fillStyle={{
-          backgroundColor: "rgba(104,106,106,1)"
-        }}
         barStyle={{
           background: "linear-gradient(97.64deg, #020305 0.45%, #0B0D0F 100%)",
           boxShadow:"0px 0px 0px 2px rgba(154,154,155,1)"
@@ -72,8 +99,103 @@ export const HorizontalStyled = () =>{
         innerCursorStyle={{backgroundColor: "rgba(240,240,240,1"}}
         outerCursorStyle={{backgroundColor: "rgba(0,0,0,0)"}}
         outerCursorSVGUrl={outerCursorSVGUrl}
+        whileHover={"highlight"}
+        whileTap={"highlight"}
+        initial={"default"}
       />
-      {"input: " + input.toFixed(4)}
+      <br/>
+      <br/>
+      <text className={"no-select"}>
+        {"input: " + input.toFixed(4)}
+      </text>
+    </div>
+  )
+};
+
+export const HorizontalOpacityStyle = () =>{ 
+  const width = 280;
+  const height = 30;
+  const borderWidth = 2;
+  const innerCursorVariants = {
+    default : {
+        boxShadow:"0px 0px 25px 2px rgba(255,255,255,.5)",
+        scaleX: 1,
+        transition: {
+          type: "tween",
+          duration: .1
+        }
+    },
+    highlight : {
+        boxShadow:"0px 0px 25px 2px rgba(255,255,255,1)",
+        scaleX: 2,
+        transition: {
+          type: "tween",
+          duration: .1
+        }
+    }
+  }
+  const outerCursorVariants = {
+      default : {
+          filter: "invert(0.0)",
+          opacity: .7,
+          scale : 1
+      },
+      highlight : {
+          filter: "invert(1)",
+          opacity: 1,   
+          scale: 2,
+          transition: {
+            type: "tween",
+            duration: .1
+          }
+      }
+  }
+  const fillVariants = {
+    default : {
+      backgroundColor: "rgba(200,200,200,.1)"
+    },
+    highlight : {
+      backgroundColor: "rgba(255,255,255,.5)"
+    }
+  }
+  const [input, setInput] = useState(1);
+  return (
+    <div>
+      <HorizontalSlider 
+        setInput={setInput} 
+        barHeight={height}
+        barWidth={width}
+        innerCursorWidth={5} 
+        outerCursorWidth={15} 
+        fillVariants={fillVariants}
+        outerCursorVariants={outerCursorVariants}
+        innerCursorVariants={innerCursorVariants}
+        barStyle={{
+          background: "rgba(0,0,0,0)",
+          boxShadow:`0px 0px 0px ${borderWidth}px rgba(154,154,155,1)`
+        }}
+        innerCursorStyle={{backgroundColor: "rgba(240,240,240,1"}}
+        outerCursorStyle={{backgroundColor: "rgba(0,0,0,0)"}}
+        outerCursorSVGUrl={outerCursorSVGUrl}
+        whileHover={"highlight"}
+        whileTap={"highlight"}
+        initial={"default"}
+      >
+        <OpacityCanvas 
+          width={width} 
+          height={height} 
+          color={[1,0,0,1]} 
+          patternColor0={[.7,.7,.7,.7]} 
+          patternColor1={[.4,.4,.4,.7]}
+          patternSize={16}
+          style={{position:"absolute", top:"0px", left:"0px"}}
+        />
+      </HorizontalSlider>
+      <br/>
+      <br/>
+      <text className={"no-select"}>
+        {"input: " + input.toFixed(4)}
+      </text>
     </div>
   )
 };
--- src/components/slider/slider-horizontal/index.tsx
+++ src/components/slider/slider-horizontal/index.tsx
@@ -1,6 +1,6 @@
 import React, {useRef} from "react";
 import {motion, Variants, PanInfo, HTMLMotionProps, useDragControls, useMotionValue, useTransform, MotionStyle} from "framer-motion";
-import myicon from "./outerCursor.svg";
+import "../../../styles/no-select.scss";
 
 interface SliderProps  extends HTMLMotionProps<"div">{
     outerCursorSVGUrl ?: string,
@@ -18,8 +18,15 @@ interface SliderProps  extends HTMLMotionProps<"div">{
     barWidth : number,
     barHeight: number,
     innerCursorWidth : number,
-    outerCursorWidth : number,
-    setInput : React.Dispatch<React.SetStateAction<number>>
+    outerCursorWidth ?: number,
+    outerCursorHeight ?: number,
+    setInput : React.Dispatch<React.SetStateAction<number>>,
+    /**
+     * Set to `true` if position and size of the element will never change.
+     * This will not use the slider's `React.Ref` as a drag constraint and 
+     * instead use the initial position and sizes of the slider as constraints,
+     * preventing some drag constraint bugs.
+     */
 }
 
 export const HorizontalSlider = (props : SliderProps) =>
@@ -29,20 +36,24 @@ export const HorizontalSlider = (props : SliderProps) =>
      *  states
      * ================================================================                                                              
      */
-
-    const canvasRef = useRef(null);
     const dragControls = useDragControls();
     const dragX = useMotionValue(null);
-    const dragXCursor1Transform = useTransform(dragX, (x)=> x-props.outerCursorWidth/2);
+    const dragXCursor1Transform = useTransform(dragX, (x)=> x);
     const fillScaleXTransform = useTransform(dragX, (x) => x/props.barWidth);
     /*
      * ================================================================
      *  css styles
      * ================================================================                                                              
+     * default styles:
+     * get injected before user styles, to provide 
+     * bare-bones style properties to see where the component will 
+     * located.
      */
-    // defaults
+    const wrapperDefaultStyle : MotionStyle = {
+        position: "relative"
+    }
     const barDefaultStyle : MotionStyle = {
-        height: "10px",
+        height: 10,
         backgroundColor:"grey"
     };
     const outerCursorDefaultStyle : MotionStyle = {
@@ -50,35 +61,59 @@ export const HorizontalSlider = (props : SliderProps) =>
     };
     const innerCursorDefaultStyle : MotionStyle = {
         backgroundColor:"white",
-        width: "15px",
-        height: "15px"
+        width: 15,
+        height: 15
     };
     const fillDefaultStyle : MotionStyle = {
         backgroundColor: "green"
     }
-    // overrides (cannot be overriden by user supplied styles)
+    /* override styles:
+     * get injected after default, and user styles
+     * to prevent user styles from interfering with required style
+     * properties.
+     */
+    const wrapperStyle : MotionStyle = {
+        width: props.barWidth,
+        height: props.barHeight
+    }
     const barOverrideStyle : MotionStyle = {
-        height: props.barHeight+"px",
-        width:props.barWidth+"px",
+        position:"absolute",
+        top:0,
+        left:0,
+        height: props.barHeight,
+        width:props.barWidth,
         overflow:"hidden"
     }
-    const cursorAnchorStyle : MotionStyle = {
-        y: -props.barHeight,
-        width:"1px",
-        height: "100%",
+    const innerCursorOverrideStyle : MotionStyle = {
+        /* set `position: "relative"` and `top: -props.barHeight`
+         * to initialize the inner cursor in the correct position.
+         * (when `x: dragX` is present `y: -props.barHeight` gets
+         * ignored until a drag event occurs, resulting in the inner 
+         * cursor being in the wrong position until the user drags
+         * the slider)
+         */
+        position: "relative",
+        top: -props.barHeight,
+        left: -props.innerCursorWidth/2,
+        x: dragX,
+        width:props.innerCursorWidth,
+        height: props.barHeight,
+        originX: .5
     }
     const outerCursorOverrideStyle : MotionStyle = {
+        position:"absolute",
+        left:-props.outerCursorWidth/2,
+        top: props.barHeight,
+        width: props.outerCursorWidth,
         x:dragXCursor1Transform,
-        /* prevents image from drifting downwards based on it's size */
+        /* set `verticalAlign: "top"` to prevent the outer cursor
+         * <img/> component from drifting away from the top of its
+         *  border at sizes below 15px.
+         */
         verticalAlign: "top",
-        width: `${props.outerCursorWidth}px`,
+        height: props.outerCursorHeight,
         originX: .5,
         originY: 0
-    }
-    const innerCursorOverrideStyle : MotionStyle = {
-        x:-props.innerCursorWidth/2,
-        width: `${props.innerCursorWidth}px`,
-        height: "100%",
     }
     const fillOverrideStyle : MotionStyle = {
         originX: 0,
@@ -87,52 +122,65 @@ export const HorizontalSlider = (props : SliderProps) =>
         scaleX: fillScaleXTransform
     }
 
+    function startDrag(event) {
+        dragControls.start(event, { snapToCursor: true })
+    }
     /*
      * ================================================================
      * components
      * ================================================================                                                              
      */
-    function startDrag(event) {
-        dragControls.start(event, { snapToCursor: true })
-    }
     return (
-        /*
-         * slider-cursor anchor is 1 pixel wide so that it interacts with framer-motion's drag attributes correctly
-         *      inner-cursor is the upper cursor set inside the slider bar
-         *      outer-cursor is the pointer indicator below the bar
-         *      slider-fill is the fill that follows the the inner-cursor
-         */
-        <motion.div id={"slider-wrapper"} {...props} whileHover={"highlight"} initial={"default"}>
+        <motion.div
+            id={"slider-wrapper"}
+            {...props}
+            style={{...wrapperDefaultStyle, ...props.style, ...wrapperStyle}}
+            /** 
+             *  "no-select" to prevent svg image from being highlighted 
+            */
+            className={"no-select"}
+        >
+            {props.children}
             <motion.div 
                 id={"slider-bar"}
+                /**
+                 *  using the proper order: 
+                 *   1. DefaultStyle will be loaded first,  
+                 *   2. props style will overload default style settings,
+                 *   3. OverrideStyle will override an user settings (aka override style settings are required)
+                */
                 style={{...barDefaultStyle, ...props.barStyle, ...barOverrideStyle}} 
-                ref={canvasRef} 
                 onMouseDown={startDrag}
+                variants={props.barVariants}
             >
-                {/* 
-                  * slider-fill must be below inner-cursor so it does not overlap it
+                {/**
+                  * slider-fill must come before inner-cursor so it is rendered beneath it in the layering order
+                  * slider-fill and slider-inner-cursor are both children of slider-bar to take advantage of
+                  * slider-bar's `overflow: hidden` css property
                   */}
                 <motion.div 
                     id={"slider-fill"} 
                     style={{...fillDefaultStyle, ...props.fillStyle, ...fillOverrideStyle}}
+                    variants={props.fillVariants}
                 />
                 <motion.div
-                    id={"slider-cursor-anchor"}
-                    style={{...cursorAnchorStyle, x:dragX}}
+                    id={"slider-inner-cursor"}
+                    style={{...innerCursorDefaultStyle, ...props.innerCursorStyle, ...innerCursorOverrideStyle}}
+                    initial={{x:props.barWidth}}
+                    /**
+                     * dragDirectionLock combined with a specific drag direction i.e. `drag={"x"}` or `drag={"y"}` 
+                     * prevents some framer-motion drag glitches
+                     */
                     dragDirectionLock
                     drag={"x"}
-                    dragConstraints={canvasRef}
+                    dragConstraints={{left:0, right:props.barWidth}}
                     dragElastic={false}
                     dragMomentum={false}
                     dragControls={dragControls}
                     dragPropagation={false}
-                    onDrag={(event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo)=>{props.setInput(info.point.x/(props.barWidth-1))}}
+                    onDrag={(event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo)=>{props.setInput(info.point.x/(props.barWidth))}}
+                    variants={props.innerCursorVariants}
                 >
-                    <motion.div 
-                        id={"slider-inner-cursor"}
-                        style={{...innerCursorDefaultStyle, ...props.innerCursorStyle, ...innerCursorOverrideStyle}}
-                        variants={props.innerCursorVariants}
-                    />
                 </motion.div>
             </motion.div>
             <motion.img
@@ -141,6 +189,11 @@ export const HorizontalSlider = (props : SliderProps) =>
                 style={{...outerCursorDefaultStyle, ...props.outerCursorStyle, ...outerCursorOverrideStyle}}
                 variants={props.outerCursorVariants}
                 width={props.outerCursorWidth}
+                /* call preventDefault to prevent dragging on svg to be interpreted as trying to drag the svg file */
+                onMouseDown={(e : React.MouseEvent<HTMLImageElement, MouseEvent>)=>{
+                    startDrag(e);
+                    e.preventDefault()
+                }}
             >
             </motion.img>
         </motion.div>
